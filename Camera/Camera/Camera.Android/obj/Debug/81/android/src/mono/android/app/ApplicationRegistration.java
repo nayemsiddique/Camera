415de2c51cd0e623bc324bd2c6ce5604f2791311
Nayem Siddique <nayemsiddiqueasif@gmail.com>
2020-01-09 13:32:13 +0600

    inital deploy

---
+++ Camera/Camera/Camera.Android/obj/Debug/81/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Camera.Droid.MainApplication, Camera.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5c72ee7b13953078342a43c21c1f47288.MainApplication.class, md5c72ee7b13953078342a43c21c1f47288.MainApplication.__md_methods);
+		
+	}
+}
---
+++ Camera/Camera/Camera/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Camera
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TakePhotoAction(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            PermissionStatus cameraStatus = await Plugin.Permissions.CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+
+
+            if (cameraStatus != PermissionStatus.Granted)
+            {
+                var permission = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Camera);
+                var status = permission[Permission.Camera];
+            }
+
+
+            PermissionStatus strogeStatus = await Plugin.Permissions.CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+
+            if (strogeStatus != PermissionStatus.Granted)
+            {
+                var permission = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Storage);
+                var status = permission[Permission.Storage];
+            }
+
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+                //Location=""
+
+
+            });
+
+            //await DisplayAlert("Path", file.Path, "ok");
+
+            if (file == null)
+                return;
+
+            myImage.Source = ImageSource.FromStream(() => file.GetStream());
+
+        }
+
+        private async void ChoosePhotoAction(object sender, EventArgs e)
+        {
+
+            await CrossMedia.Current.Initialize();
+
+            PermissionStatus strogeStatus = await Plugin.Permissions.CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+
+            if (strogeStatus != PermissionStatus.Granted)
+            {
+                var permission = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Storage);
+                var status = permission[Permission.Storage];
+                if (status == null)
+                {
+                    await DisplayAlert("File Access Error", "Need File Access", "Ok");
+                    return;
+                }
+            }
+
+
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions { });
+
+            if (file == null) return;
+
+            myImage.Source = ImageSource.FromStream(() => file.GetStream());
+
+        }
+
+    }
+}
